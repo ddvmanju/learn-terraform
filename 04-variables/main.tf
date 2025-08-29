dc4a8da81d5922523fcd7541a296fb85faf27172
Master CVV <ddv.manju@gmail.com>
2025-08-29 15:49:59 +0530

    create test vm

---
+++ 04-variables/main.tf
@@ -0,0 +1,51 @@
+variable "s1" {
+    default = "Hello"
+    }
+
+variable "n1" {
+    default = 2
+    }
+
+variable "b1" {
+    default = true
+    }
+
+# Data types that transform supports
+# 1. string
+# 2. Number
+# 3. Boolean
+
+# string only require quotes, that to be only double quotes. Terraform does not have single quotes support.
+
+variable "v1" {
+    default = "Hello"
+    }
+
+variable "v2" {
+    default = [
+        "Hello",
+        2,
+        false
+        ]
+    }
+
+variable "v3" {
+    default ={
+        course = "devops"
+        cloud = "azure"
+        }
+    }
+# variable Type that terraform supports
+# 1. plain
+# 2. List ( A key having multiple values, And each value can be a different data type)
+# 3. Map ( A key having again keys and values)
+
+## Access your variables
+output "v1" {
+    value = var.v1
+    }
+
+# while accessing variable, if we have some string combination then we can access it with ${}
+output "v2" {
+    value = "${var.v1} - John"
+    }
