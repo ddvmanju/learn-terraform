3f4249c32f10462be020467b51ac5db9af78bddd
Master CVV <ddv.manju@gmail.com>
2025-08-29 14:59:37 +0530

    create test vm

--- 03-resource/main.tf
+++ 03-resource/main.tf
@@ -30,8 +30,8 @@ resource "azurerm_virtual_machine" "main" {
   name                  = "test-vm"
   location              = data.azurerm_resource_group.example.location
   resource_group_name   = data.azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_B2s"
+  network_interface_ids = [azurerm_network_interface.example.id]
+  vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
