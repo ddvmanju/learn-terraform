c048348b24aca6e836b8016c47cde636f991c94e
Master CVV <ddv.manju@gmail.com>
2025-08-29 14:40:48 +0530

    create test vm

--- 03-resource/main.tf
+++ 03-resource/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.internal.id
+    subnet_id                     = data.azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
   }
 }
