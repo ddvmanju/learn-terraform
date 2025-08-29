e7c084466bb7687e43d4cba83c4073c35977c7b0
Master CVV <ddv.manju@gmail.com>
2025-08-29 12:38:33 +0530

    Learn Terraform - its a market leader

--- 02-datasource/main.tf
+++ 02-datasource/main.tf
@@ -5,3 +5,8 @@ data "azurerm_resource_group" "example" {
 output "rg" {
     value = data.azurerm_resource_group.example
     }
+
+provider "azurerm" {
+    features{}
+    subscription_id = "00f9828e-4aad-42e5-ac92-a3c54883cbd3"
+    }
