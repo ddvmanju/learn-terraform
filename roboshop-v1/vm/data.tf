38b6c383a567639101ad0ec1fdf78d01fdb763e5
Master CVV <ddv.manju@gmail.com>
2025-08-30 12:49:07 +0530

    roboshop project sample version

--- roboshop-v1/vm/data.tf
+++ roboshop-v1/vm/data.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "example" {
 }
 
 data "azurerm_subnet" "example" {
-  name                 = "Default"
+  name                 = "Frontend-vnet/default"
   virtual_network_name = "project-setup-network"
   resource_group_name  = data.azurerm_resource_group.example.name
 
