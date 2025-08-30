6606f1e7985ffbadb5da632e41150cec2c535136
Master CVV <ddv.manju@gmail.com>
2025-08-30 12:07:32 +0530

    roboshop project sample version

--- 06-module/main.tf
+++ 06-module/main.tf
@@ -1,4 +1,9 @@
 #module is group of resources
 module "sample" {
     source ="./sample"
+    x = var.x
+    y = var.y
     }
+
+variable "x" {}
+variable "y" {}
---
+++ roboshop-v1/provider.tf
@@ -0,0 +1,4 @@
+provider "azurerm" {
+    features{}
+    subscription_id = "00f9828e-4aad-42e5-ac92-a3c54883cbd3"
+    }
