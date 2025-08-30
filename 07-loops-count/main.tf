00bf711ae27575226fd0d588c5ec2edcf125f550
Master CVV <ddv.manju@gmail.com>
2025-08-30 15:21:14 +0530

    robo shop project sample version

--- 07-loops-count/main.tf
+++ 07-loops-count/main.tf
@@ -1,4 +1,14 @@
 resource "null_resource"  "demo" {
   count = 10
+}
 
+resource "null_resource" "demo" {
+  count = length(var.demo)
+}
+
+variable "demo" {
+  default = [
+  "apple",
+  "Orange"
+  ]
 }
