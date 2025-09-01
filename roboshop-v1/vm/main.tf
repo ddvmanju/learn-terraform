00e5a673d7618e22e8d46a07ed7d4681d59bbae4
Master CVV <ddv.manju@gmail.com>
2025-09-01 20:54:02 +0530

    updated linux image

--- roboshop-v1/vm/main.tf
+++ roboshop-v1/vm/main.tf
@@ -37,8 +37,8 @@ resource "azurerm_virtual_machine" "main" {
   delete_os_disk_on_termination = true
 
   storage_image_reference {
-    id = "/subscriptions/00f9828e-4aad-42e5-ac92-a3c54883cbd3/resourceGroups/project-setup1/providers/Microsoft.Compute/galleries/customimagedevops"
-              }
+    id = "/subscriptions/00f9828e-4aad-42e5-ac92-a3c54883cbd3/resourcegroups/project-setup1/providers/Microsoft.Compute/galleries/custompractice/images/customimage/versions/1.0.0"
+  }
 
     storage_os_disk {
       name              = var.component
