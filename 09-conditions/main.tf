4e06dfa93b5774001860b2a9f66be8e9ebcb976f
Master CVV <ddv.manju@gmail.com>
2025-09-01 14:40:15 +0530

    updated linux image

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -3,3 +3,9 @@ variable "a" {}
   output "test" {
     value = var.a > 10 ? "a is greater than 10" : "a is less than 10"
       }
+
+# these conditions with combination of loop can be used to determine whether a resource to run or not
+
+resource "null_resource" "test" {
+  count = var.a > 10 ? 1 : 0
+}
