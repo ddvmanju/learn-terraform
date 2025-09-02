b05bb0a7e52a7604aa095b01a817f281e2212d4b
Master CVV <ddv.manju@gmail.com>
2025-09-02 12:58:20 +0530

    roboshop terraform

--- roboshop-v1/vm/data.tf
+++ roboshop-v1/vm/data.tf
@@ -4,6 +4,6 @@ data "azurerm_resource_group" "example" {
 
 data "azurerm_subnet" "example" {
   name                 = "default"
-  virtual_network_name = "Frontend-vnet"
+  virtual_network_name = "DevOps-AMI-vnet" #""Frontend-vnet"
   resource_group_name  = data.azurerm_resource_group.example.name
 }
